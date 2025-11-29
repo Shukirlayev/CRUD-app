da598731001738d37bf69abd82af8cd12f3ae781
Sarvar Shukirlayev <Shukirlayevs@gmail.com>
2025-11-29 13:57:22 +0500

    New frontend initial commit

--- backend/index.js
+++ backend/index.js
@@ -11,6 +11,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+
+app.use(cors({
+  origin: "*",
+  credentials: true
+}));
+
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
